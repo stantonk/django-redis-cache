d6338722b2ae80650f9dfd3e0951dc0eee0dd648
stantonk <stantonk@gmail.com>
2012-05-21 18:49:04 -0500

    adding logging

--- redis_cache/__init__.py
+++ redis_cache/__init__.py
@@ -1,1 +1,16 @@
 from redis_cache.cache import RedisCache
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
+# ... Clean up.
+del logging
+del NullHandler
